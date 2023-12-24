feed38ca2c102613e685542647a30a82a5ee727e
josePc <jose.csousa22@gmail.com>
2023-12-24 14:18:44 -0300

    feat: SRP - Single responsability principle
    - Exemplo em codigo do SRP
    - Adicionado no README.md exemplos.

---
+++ README.md
@@ -0,0 +1,33 @@
+﻿# Princípios Solid
+
+![Solid](./Solid.png)
+
+Este repositório tem como objetivo fornecer exemplos práticos e claros que ilustram os princípios do SOLID
+
+Referências:
+https://medium.com/desenvolvendo-com-paixao/o-que-%C3%A9-solid-o-guia-completo-para-voc%C3%AA-entender-os-5-princ%C3%ADpios-da-poo-2b937b3fc530
+
+## SRP - Single responsability principle (Principio de responsabilidade única)
+
+O principio de **responsabilidade única** se refere a que uma classe só deve ter uma unica responsabilidade.
+
+Por exemplo uma classe especializada em N coisas chamado comumente de **god class**
+pode gerar os seguintes problemas.
+
+* Falta de coesão — uma classe não deve assumir responsabilidades que não são suas
+* Alto acoplamento — Mais responsabilidades geram um maior nível de dependências 
+  deixando o sistema engessado e frágil para alterações
+* Dificuldades na implementação de testes automatizados — É difícil de **“mockar”** esse tipo de classe
+* Dificuldades para reaproveitar o código
+
+Um exemplo visto no código é a classe **PedidoGodClass** onda ela pode salvar no banco, exibir, 
+enviar notificação de email.
+
+Seguindo o principo seus métodos foram divididos em 4 classes onde cara uma tem sua responsabilidade
+
+* **Pedido** - Responsável pela criação do pedido
+* **PedidoViewer** - Responsável por exibir informações sobre o pedido
+* **PedidoRepository** - Responsável por fazer interações com o banco de dados.
+* **EmailService** -  Responsável por Operações referentes a email.
+
+## OCP - Open-Closed Principle (Principo Aberto-Fechado)
--- S/SingleResponsibilityPrinciple/Program.cs
+++ S/SingleResponsibilityPrinciple/Program.cs
@@ -1,3 +1,89 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// SRP - Uma classe só deve ter um e somente um motivo para mudar
+/*
+                  Principio de responsabilidade única
+    Esse principio declara que uma classe só deve ser especializada em um assunto,
+    assim evitandos as God class, que são classes especializadas em tudo.
+    
+                Sua violação pode causa os seguintes problemas
+    * Falta de coesão — uma classe não deve assumir responsabilidades que não são suas;
+    * Alto acoplamento — Mais responsabilidades geram um maior nível de dependências,
+      deixando o sistema engessado e frágil para alterações;
+    * Dificuldades na implementação de testes automatizados — É difícil de “mockar” esse tipo de classe;
+    * Dificuldades para reaproveitar o código;
+ 
+ */
 
-Console.WriteLine("Hello, World!");
+var podeTudo = new PedidoGodClass();
+podeTudo.AdicionarPedidoAoBancoDeDados(podeTudo);
+podeTudo.ExibirPedido();
+podeTudo.EnviarConfirmacaoDeEmail();
+
+//SRP - Cada classe com sua responsibilidade
+var pedido = new Pedido();
+
+var viewer = new PedidoViewer();
+viewer.ExibirPedido(pedido);
+
+var repository = new PedidoRepository();
+repository.AdicionarPedidoAoBancoDeDados(pedido);
+
+var emailService = new EmailService();
+emailService.EnviarConfirmacaoDeEmail();
+
+
+
+// Nesse exemplo temos a god class onde o produto é responsavel por tudo
+class PedidoGodClass
+{
+    public int Numero { get; set; }
+    public DateTime Data { get; set; }
+    public double Valor { get; set; }
+
+    public void ExibirPedido()
+    {
+        Console.WriteLine("Exibindo produto: " + Numero);
+    }
+    public void AdicionarPedidoAoBancoDeDados(PedidoGodClass produto)
+    {
+        Console.WriteLine("Pedido adicionado ao banco de dados: " + produto.Numero);
+    }
+    
+    public void EnviarConfirmacaoDeEmail()
+    {
+        Console.WriteLine("E-mail de confirmação enviado");
+    }
+}
+
+// Utilizando o principio de responsabilidade unica, teriamos a divisão em 3 classes
+// Pedido, PedidoRepository, EmailService e PedidoViewer
+
+class Pedido
+{
+    public int Numero { get; set; }
+    public DateTime Data { get; set; }
+    public double Valor { get; set; }
+}
+
+class PedidoViewer
+{
+    public void ExibirPedido(Pedido pedido)
+    {
+        Console.WriteLine("Exibindo produto: " + pedido);
+    }
+}
+
+class PedidoRepository
+{
+    public void AdicionarPedidoAoBancoDeDados(Pedido pedido)
+    {
+        Console.WriteLine("Pedido adicionado ao banco de dados: " + pedido.Numero);
+    }
+}
+
+class EmailService
+{
+    public void EnviarConfirmacaoDeEmail()
+    {
+        Console.WriteLine("E-mail de confirmação enviado");
+    }
+}
